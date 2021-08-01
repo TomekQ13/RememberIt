b09db480a8d2ecf0553369bbc2a0a790b7b1126f
TomekQ13 <48492562+TomekQ13@users.noreply.github.com>
2021-08-02 00:01:28 +0200

    occurences work start

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,10 +4,7 @@ const client = require('../db.js')
 const {User, getUserByEmail, getUserById} = require('../models/user')
 
 router.get("/", async (req, res) => {
-    // const r = await client.query('select * from "user"')
-    // const usr = await getUserByEmail('test@test.com')
-    // console.log(usr)
-    // console.log(r.rows)
+    
     res.render('index')
     
 })
