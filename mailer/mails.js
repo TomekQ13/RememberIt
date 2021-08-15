3bbd86f731af634f60d428376484dabb7133efb3
TomekQ13 <48492562+TomekQ13@users.noreply.github.com>
2021-08-15 23:07:11 +0200

    mailer work

---
+++ mailer/mails.js
@@ -0,0 +1,59 @@
+const nodemailer = require('nodemailer')
+const {client} = require('./db.js')
+
+function makeTransporter() {
+    let transporter = nodemailer.createTransport({
+        host: 'mail.privateemail.com',
+        port: 465,
+        secure: true,
+        auth: {
+            user: 'contact@neverforgetit.net',
+            pass: process.env.EMAIL_PASS,
+        }
+    })
+    return transporter;
+}
+
+async function sendEmail(to, subject, text, html, transporter = makeTransporter(), from = '"Never Forget It!" <contact@neverforgetit.net>') {
+    try {
+        let r = await transporter.sendMail({
+            from: from, // sender address
+            to: to, // list of receivers
+            subject: subject, // Subject line
+            text: text, // plain text body
+            html: html, // html body
+            }); 
+        console.log("Message sent: %s", info.messageId);
+    } catch (e) {
+        console.log('There was an error while sending the email')
+        console.error(e)
+    }    
+}
+
+async function sendReminder(transporter, eventName, eventDate, to) {
+    let subject = `Never forget it! ${eventName} is coming.`
+    let text = `Hi!
+The event ${eventName} is coming on ${eventDate}.
+`
+    let html = `<h1>Hi!</h1>
+<p>The event ${eventName} is coming on ${eventDate}.</p>    
+`
+    try {
+        await sendEmail(
+            transporter = transporter,
+            to = to,
+            subject = subject,
+            text = text,
+            html = html
+        )
+    } catch (e) {
+        console.log('There was an error while sending the email')
+        console.error(e)
+    }
+}
+
+module.exports = {makeTransporter, sendEmail, sendReminder}
+
+
+    
+
