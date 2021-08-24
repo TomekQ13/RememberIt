14825786caf6a9a3f3faec12732737c28ed34ab4
TomekQ13 <48492562+TomekQ13@users.noreply.github.com>
2021-08-24 23:32:42 +0200

    insert_occurences works

--- ddl.sql
+++ ddl.sql
@@ -36,66 +36,6 @@ create table "reminder" (
     constraint fk_reminder_event foreign key(event_id) references event(customer_id) on delete cascade
 );
 
-drop materialized view occurence;	
-create materialized view occurence as (
-with recursive monthly_occurence as (
-	select e.id, e.public_id, e.name, e.description, e.user_id, e.first_date as date, e.repeat
-	from "event" e
-	where e.repeat = 'monthly'
-	and e.first_date >= now()
-	and e.first_date < now() + interval '2 year'
-	union
-	select e.id, e.public_id, e.name, e.description, e.user_id, date(e.date + interval '1 month') as date, e.repeat
-	from "monthly_occurence" e
-	where e.date + interval '1 month' < now() + interval '2 year'
-), weekly_occurence as (
-	select e.id, e.public_id, e.name, e.description, e.user_id, e.first_date as date, e.repeat
-	from "event" e
-	where e.repeat = 'weekly'
-	and e.first_date >= now()
-	and e.first_date < now() + interval '2 year'
-	union
-	select e.id, e.public_id, e.name, e.description, e.user_id, date(e.date + interval '1 week') as date, e.repeat
-	from "weekly_occurence" e
-	where e.date + interval '1 week' < now() + interval '2 year'
-), daily_occurence as (
-	select e.id, e.public_id, e.name, e.description, e.user_id, e.first_date as date, e.repeat
-	from "event" e
-	where e.repeat = 'daily'
-	and e.first_date >= now()
-	and e.first_date < now() + interval '2 year'
-	union
-	select e.id, e.public_id, e.name, e.description, e.user_id, date(e.date + interval '1 day') as date, e.repeat
-	from "daily_occurence" e
-	where e.date + interval '1 day' < now() + interval '2 year'
-), yearly_occurence as (
-	select e.id, e.public_id, e.name, e.description, e.user_id, e.first_date as date, e.repeat
-	from "event" e
-	where e.repeat = 'yearly'
-	and e.first_date >= now()
-	and e.first_date < now() + interval '2 year'
-	union
-	select e.id, e.public_id, e.name, e.description, e.user_id, date(e.date + interval '1 year') as date, e.repeat
-	from "yearly_occurence" e
-	where e.date + interval '1 year' < now() + interval '2 year'
-)
-	select e.*, r.remind_days_before, e.date - r.remind_days_before as reminder_date from (
-		select *
-		from yearly_occurence
-		union all
-		select *
-		from monthly_occurence
-		union all
-		select *
-		from weekly_occurence
-		union all
-		select *
-		from daily_occurence
-	) e left join reminder r
-		on e.public_id = r.event_public_id
-		order by reminder_date
-);
-
 drop table occurence;
 create table occurence (
 	id integer primary key generated always as identity,
@@ -117,44 +57,49 @@ with recursive monthly_occurence as (
 	select e.id, e.public_id, e.name, e.description, e.user_id, e.first_date as date, e.repeat
 	from "event" e
 	where e.repeat = 'monthly'
-	and e.first_date >= now()
-	and e.first_date < now() + interval '1 day' * period_days
+	and e.first_date <= current_date + interval '1 day' * period_days
 	union
 	select e.id, e.public_id, e.name, e.description, e.user_id, date(e.date + interval '1 month') as date, e.repeat
 	from "monthly_occurence" e
-	where e.date + interval '1 month' < now() + interval '2 year'
+	where e.date + interval '1 month' <= current_date + interval '1 day' * period_days
 ), weekly_occurence as (
 	select e.id, e.public_id, e.name, e.description, e.user_id, e.first_date as date, e.repeat
 	from "event" e
 	where e.repeat = 'weekly'
-	and e.first_date >= now()
-	and e.first_date < now() + interval '1 day' * period_days
+	and e.first_date <= current_date + interval '1 day' * period_days
 	union
 	select e.id, e.public_id, e.name, e.description, e.user_id, date(e.date + interval '1 week') as date, e.repeat
 	from "weekly_occurence" e
-	where e.date + interval '1 week' < now() + interval '2 year'
+	where e.date + interval '1 week' <= current_date + interval '1 day' * period_days
 ), daily_occurence as (
 	select e.id, e.public_id, e.name, e.description, e.user_id, e.first_date as date, e.repeat
 	from "event" e
 	where e.repeat = 'daily'
-	and e.first_date >= now()
-	and e.first_date < now() + interval '1 day' * period_days
+	and e.first_date <= current_date + interval '1 day' * period_days
 	union
 	select e.id, e.public_id, e.name, e.description, e.user_id, date(e.date + interval '1 day') as date, e.repeat
 	from "daily_occurence" e
-	where e.date + interval '1 day' < now() + interval '2 year'
+	where e.date + interval '1 day' <= current_date + interval '1 day' * period_days
 ), yearly_occurence as (
 	select e.id, e.public_id, e.name, e.description, e.user_id, e.first_date as date, e.repeat
 	from "event" e
 	where e.repeat = 'yearly'
-	and e.first_date >= now()
-	and e.first_date < now() + interval '1 day' * period_days
+	and e.first_date <= current_date + interval '1 day' * period_days
 	union
 	select e.id, e.public_id, e.name, e.description, e.user_id, date(e.date + interval '1 year') as date, e.repeat
 	from "yearly_occurence" e
-	where e.date + interval '1 year' < now() + interval '1 day' * period_days
+	where e.date + interval '1 year' <= current_date + interval '1 day' * period_days
 )
-	select e.*, r.remind_days_before, e.date - r.remind_days_before as reminder_date from (
+	insert into occurence(public_id, user_id, name, description, date, repeat, remind_days_before, reminder_date, email)
+	select e.public_id,
+	e.user_id,
+	e.name,
+	e.description,
+	e.date,
+	e.repeat,
+	r.remind_days_before,
+	e.date - r.remind_days_before as reminder_date,
+	u.email from (
 		select *
 		from yearly_occurence
 		union all
@@ -168,7 +113,9 @@ with recursive monthly_occurence as (
 		from daily_occurence
 	) e left join reminder r
 		on e.public_id = r.event_public_id
-		order by reminder_date
+		left join "user" u
+		on e.user_id = u.id		
+ 		where e.date - r.remind_days_before >= current_date
 ;
 $$;
 	
