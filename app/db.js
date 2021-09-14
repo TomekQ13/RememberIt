7b0448bfb53f0652ae608af2e6b790a9d9ae4d65
TomekQ13 <48492562+TomekQ13@users.noreply.github.com>
2021-09-14 22:19:50 +0200

    added console.log

--- app/db.js
+++ app/db.js
@@ -1,5 +1,11 @@
 const { Client } = require('pg')
-
+console.log(`
+    user: ${process.env.POSTGRES_USER},
+    host: ${process.env.POSTGRES_HOST},
+    database: ${process.env.POSTGRES_DATABASE},
+    password: ${process.env.POSTGRES_PASSWORD},
+    port: ${process.env.POSTGRES_PORT}
+`)
 const client = new Client({
     user: process.env.POSTGRES_USER,
     host: process.env.POSTGRES_HOST,
