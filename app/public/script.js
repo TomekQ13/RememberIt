f1b96c6ea6f336ba65e8265207994304ce2a6820
TomekQ13 <48492562+TomekQ13@users.noreply.github.com>
2021-09-23 23:33:02 +0200

    reminder days hide or show

--- app/public/script.js
+++ app/public/script.js
@@ -1,1 +1,15 @@
-const smsRequireFormField = document.getElementById('')
+const reminderSMS = document.getElementById('reminderSMS')
+const smsReminderFormGroup = document.getElementsByName('smsReminderFormGroup')
+
+if (reminderSMS) {
+    reminderSMS.addEventListener('change', (el, i) => {
+        smsReminderFormGroup.forEach(el => {
+            if (reminderSMS.checked) {
+                el.classList.remove('d-none')
+            } else {
+                el.classList.add('d-none')
+            }            
+        })
+    })
+
+}
