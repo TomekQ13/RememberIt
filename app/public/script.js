6abbbf8907077a0a29763b3e41a699bd669b309c
TomekQ13 <48492562+TomekQ13@users.noreply.github.com>
2021-09-30 22:03:14 +0200

    possibility to add more email reminders added

--- app/public/script.js
+++ app/public/script.js
@@ -11,5 +11,51 @@ if (reminderSMS) {
             }            
         })
     })
-
 }
+
+const reminderEmail = document.getElementById('reminderEmail')
+const emailReminderFormGroup = document.getElementsByName('emailReminderFormGroup')
+
+if (reminderEmail) {
+    reminderEmail.addEventListener('change', () => {
+        emailReminderFormGroup.forEach(el => {
+            if (reminderEmail.checked) {
+                el.classList.remove('d-none')
+            } else {
+                el.classList.add('d-none')
+            }            
+        })
+    })
+}
+
+const btnAddEmailReminder = document.getElementById('btnAddEmailReminder')
+if (btnAddEmailReminder) {
+    btnAddEmailReminder.addEventListener('click', () => {
+        btnAddEmailReminder.insertAdjacentHTML('beforebegin', 
+        `
+        <label for="remind_days_before_email">How many days before would you like to get an email reminder?</label>
+        <input type="number" id="remind_days_before_email" name="remind_days_before_email" min="1" max="365" required>
+        <button type="button" class="btn btn-outline-danger btn-sm" name="btnRemoveEmailReminder">+</button><br>
+        `        
+        )
+        const btnRemoveEmailReminders = document.getElementsByName('btnRemoveEmailReminder')
+        if (btnRemoveEmailReminders) {
+            addRemoveEmailReminderEL([btnRemoveEmailReminders[btnRemoveEmailReminders.length -1]])
+        }        
+    })
+}
+
+function addRemoveEmailReminderEL(elements) {
+    elements.forEach(el => {
+        el.addEventListener('click', () => {
+            el.previousElementSibling.remove()
+            el.previousElementSibling.remove()
+            el.previousElementSibling.remove()
+            el.remove()
+        })
+    })
+}
+
+const btnRemoveEmailReminders = document.getElementsByName('btnRemoveEmailReminder')
+addRemoveEmailReminderEL(btnRemoveEmailReminders)
+
